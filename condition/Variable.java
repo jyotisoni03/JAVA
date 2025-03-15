b0a83d02bb942744b3055f386c0f6dabdd6bbb4c
JYOTI SONI <jyoti@JYOTIs-MacBook-Air.local>
2025-03-15 14:51:05 +0530

    basics of java

---
+++ condition/Variable.java
@@ -0,0 +1,14 @@
+// package condition;
+
+// import java.util.Scanner;
+
+// public class Variable {
+//     public static void main(String args[]){
+//         int button = Scanner.
+//         }
+
+
+
+//     }
+    
+// }
