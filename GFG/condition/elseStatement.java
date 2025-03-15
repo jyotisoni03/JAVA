6667a0e7e99276029ae32e6ba30fa1e68721593b
JYOTI SONI <jyoti@JYOTIs-MacBook-Air.local>
2025-03-15 15:52:03 +0530

    solve

---
+++ GFG/condition/elseStatement.java
@@ -0,0 +1,20 @@
+package GFG.condition;
+
+public class elseStatement {
+    public static void utility(int number){
+        //Write your if-else statement below
+        if(number > 100){
+                System.out.println("Big");
+            }
+            
+        
+        
+        //Write your if-else statement above
+        else{
+                System.out.println("Number");
+            }
+            
+    }
+
+    
+}
