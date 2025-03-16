2948005af7cc78bcbc34495eb784d7b63deb4dfe
JYOTI SONI <jyoti@JYOTIs-MacBook-Air.local>
2025-03-16 22:55:58 +0530

    solve

---
+++ Loops/loops.java
@@ -0,0 +1,27 @@
+package Loops;
+
+
+
+public class loops {
+
+    public static void main(String args[]) {
+    //    for(int i = 0 ; i < 11 ;  i++){
+    //        System.out.println(i);
+    //    }
+int i = 0; 
+//    while ( i< 11){
+//        System.out.println(i);
+//        i = i+1; // i++
+//    }
+
+do { 
+   System.out.println(i);
+   i++;
+} while (i<=11);
+
+
+}
+}
+
+    
+
---
+++ Loops/sum.java
@@ -0,0 +1,14 @@
+package Loops;
+import.java.util.*;
+
+public class sum {
+    public static void main(string arg[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc nextInt();
+        for(int i = 0; i<+n; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+    
+}
